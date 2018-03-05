7a003bf5fec49e649f5a9189a9b6fe6ceb7007ee
OsipovaAnna <19ankaosipova98@gmail.com>
2018-03-05 23:50:13 +0300

    Admin: article update

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use App\Article;
 use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
 class ArticleController extends Controller
 {
     /**
@@ -14,44 +11,40 @@ class ArticleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-     public function index()
-     {
-         return view('admin.articles.index', [
-           'articles' => Article::orderBy('created_at', 'desc')->paginate(10)
-         ]);
-     }
-
-
+    public function index()
+    {
+        return view('admin.articles.index', [
+          'articles' => Article::orderBy('created_at', 'desc')->paginate(10)
+        ]);
+    }
     /**
-     * Show the form for creating a new resource.
+   * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-     public function create()
-     {
-         return view('admin.articles.create', [
-            'article'    => [],
-            'categories' => Category::with('children')->where('parent_id', 0)->get(),
-            'delimiter'  => ''
-          ]);
-     }
-
+    public function create()
+    {
+      return view('admin.articles.create', [
+         'article'    => [],
+         'categories' => Category::with('children')->where('parent_id', 0)->get(),
+         'delimiter'  => ''
+        ]);
+    }
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-     public function store(Request $request)
-     {
-     $article = Article::create($request->all());
-     // Categories
-     if ($request->input('categories')) {
-         $article->categories()->attach($request->input('categories'));
-     }
-     return redirect()->route('admin.article.index');
-   }
-   
+    public function store(Request $request)
+    {
+              $article = Article::create($request->all());
+        // Categories
+        if ($request->input('categories')) {
+            $article->categories()->attach($request->input('categories'));
+        }
+        return redirect()->route('admin.article.index');
+    }
     /**
      * Display the specified resource.
      *
@@ -62,7 +55,6 @@ class ArticleController extends Controller
     {
         //
     }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -71,29 +63,37 @@ class ArticleController extends Controller
      */
     public function edit(Article $article)
     {
-        //
+      return view('admin.articles.edit', [
+          'article'    => $article,
+          'categories' => Category::with('children')->where('parent_id', 0)->get(),
+          'delimiter'  => ''
+        ]);
     }
-
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Article  $article
+   * @param  \App\Article  $article
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Article $article)
     {
-        //
+      $article->update($request->except('slug'));
+        // Categories
+        $article->categories()->detach();
+        if ($request->input('categories')) {
+            $article->categories()->attach($request->input('categories'));
+        }
+        return redirect()->route('admin.article.index');
     }
-
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Article  $article
+   * @param  \App\Article  $article
      * @return \Illuminate\Http\Response
      */
     public function destroy(Article $article)
     {
-        //
+      //
     }
 }
