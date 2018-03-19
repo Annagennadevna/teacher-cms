e4acda9d0bb486701a26aecaf8a4a73b5d9064d9
Annagennadevna <19ankaosipova98@gmail.com>
2018-03-19 20:51:12 +0300

    Enable/disable user registration in config

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 Route::get('/blog/category/{slug?}', 'BlogController@category')->name('category');
 Route::get('/blog/article/{slug?}', 'BlogController@article')->name('article');
 Route::get('/blog/search', 'BlogController@search')->name('search');
+
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth'] ], function () {
     Route::get('/', 'DashboardController@dashboard')->name('admin.index');
     Route::resource('/category', 'CategoryController', ['as'=>'admin']);
@@ -20,8 +21,31 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['aut
         Route::resource('/user', 'UserController', ['as' => 'admin.user_management']);
     });
 });
+
 Route::get('/', function () {
     return view('blog.home');
 });
-Auth::routes();
+
+// Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+if (config('auth.registration')) {
+    $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    $this->post('register', 'Auth\RegisterController@register');
+} else {
+    Route::match(['get','post'], 'register', function () {
+        return redirect()->route('login');
+    })->name('register');
+}
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
 Route::get('/home', 'HomeController@index')->name('home');
